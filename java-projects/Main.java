cf212bed15763eeb516f6d8a78bc467861d6463b
TumisangMalefo-BIT-tech <aobakwetumisang67@gmail.com>
2026-01-10 14:02:36 +0200

    Added Java project file (Main.java)

---
+++ java-projects/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Aobakwe";
+        int age = 20;
+        String course = "Bachelor of IT in Business Systems";
+
+        System.out.println("=== Student Information System ===");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Course: " + course);
+        System.out.println();
+        System.out.println("Status: Active Student");
+    }
+}
